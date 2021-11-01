bab3c5f20dad87d3f9d3ce3b345f3bbde0866298
Nick <nick.burkard@gmail.com>
2021-11-01 15:07:55 -0400

    Add new generated files that failed to be added previously

---
+++ modules/ssm/src/main/scala/io/burkard/cdk/services/ssm/ParameterDataType.scala
@@ -0,0 +1,16 @@
+package io.burkard.cdk.services.ssm
+
+sealed abstract class ParameterDataType(val underlying: software.amazon.awscdk.services.ssm.ParameterDataType)
+  extends Product
+    with Serializable
+
+object ParameterDataType {
+  implicit def toAws(value: ParameterDataType): software.amazon.awscdk.services.ssm.ParameterDataType =
+    Option(value).map(_.underlying).orNull
+
+  case object Text
+    extends ParameterDataType(software.amazon.awscdk.services.ssm.ParameterDataType.TEXT)
+
+  case object AwsEc2Image
+    extends ParameterDataType(software.amazon.awscdk.services.ssm.ParameterDataType.AWS_EC2_IMAGE)
+}
