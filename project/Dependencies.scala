7eac02fcf6c2cfa7083e9fb90cc2a6e1943a0d89
Nick <nick.burkard@gmail.com>
2021-11-28 11:04:00 -0500

    rc33

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Aws {
-    val cdk: ModuleID = "software.amazon.awscdk" % "aws-cdk-lib" % "2.0.0-rc.31"
+    val cdk: ModuleID = "software.amazon.awscdk" % "aws-cdk-lib" % "2.0.0-rc.33"
 
     val constructs: ModuleID = "software.constructs" % "constructs" % "10.0.9"
   }
