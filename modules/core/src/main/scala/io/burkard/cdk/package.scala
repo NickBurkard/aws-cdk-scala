9e7c6f2b3251ce5b6aa2e84f2ad48e18d38a8252
Michael Mienko <michael.mienko@disneystreaming.com>
2021-11-29 11:17:16 -0500

    Handle edge case

--- modules/core/src/main/scala/io/burkard/cdk/package.scala
+++ modules/core/src/main/scala/io/burkard/cdk/package.scala
@@ -43,13 +43,11 @@ package object cdk {
         case TimeUnit.MICROSECONDS | TimeUnit.NANOSECONDS =>
           throw new IllegalArgumentException("AWS duration doesn't support time units smaller than milliseconds")
 
-        case x =>
-          // Should never be go here. TimeUnit is Java enum so Scala compiler cannot prove an exhaustive search.
-          throw new IllegalArgumentException(s"Timeunit ${x.toString} not recognized.")
-
-        case null =>
-          // Should never go here, Scala 3 compiler quirk with FiniteDuration and Java values.
-          throw new IllegalStateException("FiniteDuration does a runtime null check on Timeunit. The Scala 3 compiler doesn't see this and forces null check.")
+        case _ =>
+          /*
+          Should never reach here. TimeUnit is Java enum so Scala compiler cannot prove an exhaustive search.
+           */
+          throw new IllegalArgumentException(s"Timeunit, ${value.unit.toString}, not recognized.")
       }
 
   }
