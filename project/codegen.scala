dd95496597c0d14a8a2bd35cb4cda9fa7fe4e5db
Nick <nick.burkard@gmail.com>
2021-12-16 11:29:49 -0500

    `then` is a reserved word

--- project/codegen.scala
+++ project/codegen.scala
@@ -45,7 +45,7 @@ object codegen {
     Set(
       "abstract", "case", "catch", "class", "def", "do", "else", "extends", "false", "final", "finally", "for",
       "forSome", "if", "implicit", "import", "lazy", "match", "new", "null", "object", "override", "package",
-      "private", "protected", "return", "sealed", "super", "this", "throw", "trait", "try", "true", "type",
+      "private", "protected", "return", "sealed", "super", "then", "this", "throw", "trait", "try", "true", "type",
       "val", "var", "while", "with", "yield"
     )
 
