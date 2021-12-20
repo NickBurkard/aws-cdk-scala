e6dbfbde54dc187f81c36a536984b51b578095e0
Scala Steward <me@scala-steward.org>
2021-12-20 19:39:46 +0100

    Update constructs to 10.0.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
   object Aws {
     val cdk: ModuleID = "software.amazon.awscdk" % "aws-cdk-lib" % "2.2.0"
 
-    val constructs: ModuleID = "software.constructs" % "constructs" % "10.0.12"
+    val constructs: ModuleID = "software.constructs" % "constructs" % "10.0.13"
   }
 }
