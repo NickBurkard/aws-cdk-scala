f77bf82724ddda5638eebf9a19457e4ae37b8d90
Scala Steward <me@scala-steward.org>
2021-12-28 04:29:58 +0100

    Update constructs to 10.0.16

--- project/project/MetaDependencies.scala
+++ project/project/MetaDependencies.scala
@@ -4,7 +4,7 @@ object MetaDependencies {
   object Aws {
     val cdk: ModuleID = "software.amazon.awscdk" % "aws-cdk-lib" % "2.3.0"
 
-    val constructs: ModuleID = "software.constructs" % "constructs" % "10.0.15"
+    val constructs: ModuleID = "software.constructs" % "constructs" % "10.0.16"
   }
 
   object Google {
