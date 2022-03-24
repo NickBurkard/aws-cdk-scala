fb40be6293a7fef414cf25f59019e54c3187d864
Scala Steward <me@scala-steward.org>
2022-03-24 14:37:25 +0100

    Update aws-cdk-lib to 2.17.0

--- project/project/MetaDependencies.scala
+++ project/project/MetaDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object MetaDependencies {
   object Aws {
-    val cdk: ModuleID = "software.amazon.awscdk" % "aws-cdk-lib" % "2.16.0"
+    val cdk: ModuleID = "software.amazon.awscdk" % "aws-cdk-lib" % "2.17.0"
 
     val constructs: ModuleID = "software.constructs" % "constructs" % "10.0.90"
   }
