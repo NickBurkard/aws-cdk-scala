564f82d52060e294c67288d74b5ec5306e252c6f
Scala Steward <scala_steward@virtuslab.com>
2022-07-10 00:02:03 +0000

    Update aws-cdk-lib to 2.28.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Aws {
-    val cdk: ModuleID = "software.amazon.awscdk" % "aws-cdk-lib" % "2.28.0"
+    val cdk: ModuleID = "software.amazon.awscdk" % "aws-cdk-lib" % "2.28.1"
 
     val constructs: ModuleID = "software.constructs" % "constructs" % "10.1.41"
   }
